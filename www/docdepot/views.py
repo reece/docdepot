12397120eb916522921c3d878a173a478c9e1a7d
Reece Hart <reece@invitae.com>
2011-02-08 17:16:48 -0800

    initial django app

--- www/docdepot/views.py
+++ www/docdepot/views.py
@@ -1,1 +1,14 @@
 # Create your views here.
+
+from django.http import HttpResponse
+from django.template import Context
+from django.template.loader import get_template
+
+def pubmed(request):
+	template = get_template('pubmed.html')
+	variables = Context({
+		'page_title': 'PubMed Lookup',
+		})
+	output = template.render(variables)
+	return HttpResponse(output)
+
--- www/urls.py
+++ www/urls.py
@@ -1,16 +1,21 @@
 from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
+
+from docdepot.views import *
 
 urlpatterns = patterns('',
     # Example:
     # (r'^www/', include('www.foo.urls')),
+					   
+	(r'^pubmed/$', pubmed),				
+	(r'^login/', 'django.contrib.auth.views.login'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
 )
