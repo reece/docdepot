e15e38cad540fab26b774cd3a24d76cbd01f9204
Reece Hart <reece@invitae.com>
2011-02-11 11:05:00 -0800

    updated settings; added .gitignore

--- www/settings.py
+++ www/settings.py
@@ -109,7 +109,7 @@ AUTHENTICATION_BACKENDS = (
 		
 OPENID_CREATE_USERS = True
 OPENID_UPDATE_DETAILS_FROM_SREG = True
-#OPENID_SSO_SERVER_URL = 'https://login.launchpad.net/'
+OPENID_SSO_SERVER_URL = 'https://login.launchpad.net/'
 #OPENID_SSO_SERVER_URL = 'https://www.google.com/accounts/o8/id'
 OPENID_SSO_SERVER_URL = 'https://www.google.com/accounts/o8/ud'
 #OPENID_SSO_SERVER_URL = 'https://www.google.com/accounts/o8/site-xrds?hd=locusdev.net'
